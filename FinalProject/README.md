f99076ea65b240ae2cf458911dd72360eab5f0e7
wirtdn <wirtdn@mail.uc.edu>
2019-12-08 23:02:36 -0500

    Final Project

---
+++ FinalProject/README.md
@@ -0,0 +1,9 @@
+# Final Project
+
+For my final project I decided to go back to the calculators, for some reason they fascinate me once they work. I made a script in python that creates a "smart calculator." It can do the basic functions of Add, Multiply, Subtract, and Divide, but it can also do more like finding the Least common multiplier of two numbers, the Highest common Factor of two numbers, and also show the remainder or 'Mod' of two number after they are divided. I learned a few new terms and their functions while writing this script with the help of a few different guides.
+
+## The code
+If you look at the code you can see the different comments in for the different sections explaining what the code below it is doing.
+
+## How to use the Smart Calculator
+To use the calculator you have to type out what you want it to do for example, to do multiplication you can type "Find the multiplication of 2 and 2" and the calculator would give you the outcome. you can also use the words (Multiply or Product) and you would get the same result. Each function has a few terms that could be used. Then if you want to end the script you just simply type the word "end", then it'll prompt you again to hit emter.
---
+++ FinalProject/SmartCalc.py
@@ -0,0 +1,95 @@
+response=['Welcome to Smart Calculator!','Devan is my name.', 
+          'Thanks for using me! ','Sorry ,this is  beyond my ability.'] 
+  
+# fetching tokens from the text command 
+def extract_from_text(text): 
+    l=[] 
+    for t in text.split(' '): 
+        try: 
+            l.append(float(t)) 
+        except ValueError: 
+            pass
+    return l 
+  
+# calculating LCM (Least Common Multiple)
+def lcm(a,b): 
+    L=a if a>b else b 
+    while L<=a*b: 
+        if L%a==0 and L%b==0: 
+            return L 
+        L+=1
+  
+# calculating HCF (Highest Common Factor)
+def hcf(a,b): 
+    H=a if a<b else b 
+    while H>=1: 
+        if a%H==0 and b%H==0: 
+            return H 
+        H-=1
+  
+# Addition 
+def add(a,b): 
+    return a+b 
+  
+# Subtraction 
+def sub(a,b): 
+    return a-b 
+  
+# Multiplication 
+def mul(a,b): 
+    return a*b 
+  
+# Division 
+def div(a,b): 
+    return a/b 
+  
+# Remainder 
+def mod(a,b): 
+    return a%b 
+  
+# Response to command 
+# printing - "Thanks for using me" on exit 
+def end(): 
+    print(response[2]) 
+    input('press enter key to exit') 
+    exit() 
+   
+def myname(): 
+    print(response[1]) 
+def sorry(): 
+    print(response[3]) 
+   
+# Operations that are defined by input 
+operations={'ADD':add,'PLUS':add,'SUM':add,'ADDITION':add, 
+            'SUB':sub,'SUBTRACT':sub, 'MINUS':sub, 
+            'DIFFERENCE':sub,'LCM':lcm,'HCF':hcf, 
+            'PRODUCT':mul, 'MULTIPLY':mul,'MULTIPLICATION':mul, 
+            'DIVISION':div,'MOD':mod,'REMANDER'
+            :mod,'MODULAS':mod} 
+  
+# commands 
+commands={'NAME':myname,'EXIT':end,'END':end,'CLOSE':end} 
+           
+print('--------------'+response[0]+'------------') 
+print('--------------'+response[1]+'--------------------') 
+   
+   
+while True: 
+    print() 
+    text=input('Enter your queries:  ') 
+    for word in text.split(' '): 
+        if word.upper() in operations.keys(): 
+            try: 
+                l = extract_from_text(text) 
+                r = operations[word.upper()] (l[0],l[1]) 
+                print(r) 
+            except: 
+                print('something went wrong going plz enter again !!') 
+            finally: 
+                      break
+        elif word.upper() in commands.keys(): 
+                      commands[word.upper()]() 
+                      break
+    else:          
+        sorry() 
+ 
